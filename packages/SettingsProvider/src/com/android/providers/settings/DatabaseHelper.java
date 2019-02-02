c5fbf4e110b8d877ca52f991485f64b0d091c623
bzhpowa <f4bi1@free.fr>
2019-02-02 17:15:22 +0100

    mes settings
    Change-Id: I2191e9854c0977ff7bb452ee0ed020401e61eb76

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2353,6 +2353,8 @@ class DatabaseHelper extends SQLiteOpenHelper {
                 R.fraction.def_window_animation_scale, 1);
         loadFractionSetting(stmt, Settings.System.TRANSITION_ANIMATION_SCALE,
                 R.fraction.def_window_transition_scale, 1);
+        loadFractionSetting(stmt, Settings.System.ANIMATOR_DURATION_SCALE,
+                R.fraction.def_animator_duration_scale, 1);
     }
 
     private void loadDefaultHapticSettings(SQLiteStatement stmt) {
